d2e2c329057222de2f776bc2aa9df51c09ecb288
pecanhq <108305153+pecanhq@users.noreply.github.com>
2022-07-01 10:29:27 +1200

    Adding a python client

---
+++ python/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from os import path
+
+parent = path.abspath(path.dirname(__file__))
+with open(path.join(parent, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pecanhq',
+    version='0.0.1',
+    description="This client library unlocks Pecan IAM for server applications.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='iam authorization authentication',
+    project_urls={},
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    python_requires='>=3.5,<4',
+)
